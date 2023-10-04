85c57b1a18a7ad2296211e4701d0684edeaaf773
Ej Myles <ejelymyles@gmail.com>
2023-10-03 22:45:59 -0700

    finish sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+        
+    if length <= 0:
+        print("[]")
+        return
+
+    a, b = 0, 1
+
+   
+    fibonacci_sequence = []
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)  
+        a, b = b, a + b               
+
+  
+    print(fibonacci_sequence)
     pass
